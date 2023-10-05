b26963f4037d615248df108afd4246b0083faf2a
Ali Sharifi <ali.sharifi@hotmail.com.au>
2023-10-05 22:38:47 +1030

    added playRound function

--- index.js
+++ index.js
@@ -12,4 +12,35 @@ function getComputerChoice() {
     return message
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    let result = ""
+    if (playerSelection == "rock" && computerSelection == "Scissors") {
+        result = "You win! Rock beats Scissors";
+    }
+    else if (playerSelection == "rock" && computerSelection == "Paper") {
+        result = "You lose! Paper beats Rock"
+    }
+    else if (playerSelection == "scissors" && computerSelection == "Paper") {
+        result = "You win! Scissors beats Paper"
+    }
+    else if (playerSelection == "scissors" && computerSelection == "Rock") {
+        result = "You lose! Rock beats Scissors"
+    }
+    else if (playerSelection == "paper" && computerSelection == "Scissors") {
+        result = "You lose! Scissors beats Paper"
+    }
+    else if (playerSelection == "paper" && computerSelection == "Rock") {
+        result = "You win! Paper beats Rock"
+    }
+    else {
+        result = "You draw!"
+    }
+    return result;
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection))
